9b16bd9311c31a8915de56f780a2a65d8735546a
AtXzM <2811252742@qq.com>
2022-08-06 17:59:38 +0800

    项目初始结构创建

---
+++ glmall-commodit/src/main/java/com/atxin/glmall/commodit/GlmallCommoditApplication.java
@@ -0,0 +1,13 @@
+package com.atxin.glmall.commodit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmallCommoditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlmallCommoditApplication.class, args);
+    }
+
+}
---
+++ glmall-commodit/src/test/java/com/atxin/glmall/commodit/GlmallCommoditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atxin.glmall.commodit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmallCommoditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
